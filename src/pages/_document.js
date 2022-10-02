03810764ee39cb0d984237130569eaf9da36a53f
Greg V <greg.vannoni@gmail.com>
2022-10-02 09:02:07 -0700

    fbpx

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,29 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src={`https://www.facebook.com/tr?id=340533780623242&ev=PageView&noscript=1`}
+                    />
+                </noscript>
+
+                <script dangerouslySetInnerHTML={{
+                    __html: `!function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '340533780623242');
+                    fbq('track', 'PageView');`
+                }}
+                />
+
                 {/* <!-- Google tag (gtag.js) --> */}
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-EM3BV6M5EF"></script>
                 <script dangerouslySetInnerHTML={{
