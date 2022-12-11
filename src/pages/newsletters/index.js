4182496ef4a60a19cbb7d1b30b8a73f6b9684241
kaus0311 <knegi2@asu.edu>
2022-12-11 12:58:39 -0700

    logs

--- src/pages/newsletters/index.js
+++ src/pages/newsletters/index.js
@@ -3,16 +3,22 @@ import React from "react";
 import reactDom from "react-dom";
 
 export default function newsletters(){
+    const print_data = (event)=>{
+        // event.preventDefault();
+        console.log(event.target.header.value)
+        console.log(event.target.content.value)
+
+    }
    return(
-           <form id="newsletterForm">
+           <form id="newsletterForm" onSubmit={print_data}>
                <img src="ohack.png" alt="Opportunity Hack"/> 
                <label class="title"> Enter your title:</label><br></br>
-                <input type= "Header" /><br></br>
-                <label class="contents">Enter your contents:<br></br></label>
-                <textarea id="w3review" name="w3review" rows="4" cols="50">
+                <input type= "Header" name='header'/><br></br>
+                <label class="contents" id = 'getContent'>Enter your contents:<br></br></label>
+                <textarea id="txtArea" name="content" rows="4" cols="50">
                     At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
                     </textarea>
-                <button type="button"> "Send" </button>
+                <button type="submit"> "Send" </button>
            </form>
    )
 }
