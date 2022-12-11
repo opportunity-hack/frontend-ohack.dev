a8b1a1b1ee8a19e5bf7af98ec88cba191502171e
Nonso Okonkwo <nonsookonkwo@nonsos-air.3521.dhcp.asu.edu>
2022-12-11 11:35:22 -0700

    init

--- src/pages/newsletters/index.js
+++ src/pages/newsletters/index.js
@@ -4,17 +4,15 @@ import reactDom from "react-dom";
 
 export default function newsletters(){
    return(
-    //    <div>
            <form id="newsletterForm">
-               <label> Enter your title:</label><br></br>
+               <img src="ohack.png" alt="Opportunity Hack"/> 
+               <label class="title"> Enter your title:</label><br></br>
                 <input type= "Header" /><br></br>
-                <label>Enter your contents:<br></br></label>
+                <label class="contents">Enter your contents:<br></br></label>
                 <textarea id="w3review" name="w3review" rows="4" cols="50">
                     At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
                     </textarea>
                 <button type="button"> "Send" </button>
            </form>
-    //    </div>
-
    )
 }
