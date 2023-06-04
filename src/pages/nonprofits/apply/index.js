4ab8d71a3d0ddd20b74689f0ec4516d1323d4e9a
Greg V <greg.vannoni@gmail.com>
2023-06-04 12:15:36 -0700

    Another meta tag try

--- src/pages/nonprofits/apply/index.js
+++ src/pages/nonprofits/apply/index.js
@@ -5,28 +5,46 @@ const NonProfitApply = dynamic(() =>  import('../../../components/NonProfitApply
     ssr: false
 });
 
+
+
 export default function Apply() {
-    return (
-        <div>
-        <Head>
-            <title>Nonprofit Application for Opportunity Hack 2023</title>
-            <meta
-            name="description"
-            content="Have a problem where you think software could help? Submit your application today! We'll match you with a team of developers to help you solve your problem."
-            />
-            <meta
-            property="og:title"
-            content="Nonprofit Application for Opportunity Hack 2023"
-            />
-            <meta
-            property="og:description"
-            content="Have a problem where you think software could help? Submit your application today! We'll match you with a team of developers to help you solve your problem."
-            />
-            <meta property="og:image" content="https://i.imgur.com/hE3wm7M.png" />
-            <meta property="og:url" content="https://www.ohack.org/nonprofits/apply" />
-            <meta name="twitter:card" content="summary_large_image" />   
-        </Head>
-        <NonProfitApply/>
-        </div>
+    
+
+    return (     
+        <NonProfitApply/>     
     );
 }
+
+export const getStaticProps = async () => {
+    //const data = await fetch('/api/metadata');
+    //const metadata = await data.json();
+ 
+
+    return {
+        props: {
+            title: "Nonprofit Application for Opportunity Hack 2023",
+            description: "Have a problem where you think software could help? Submit your application today! We'll match you with a team of developers to help you solve your problem.",
+            openGraphData: [
+                {
+                    name: "title",
+                    content: "Nonprofit Application for Opportunity Hack 2023",
+                    key: "title"
+                },
+                {
+                    name: "image",
+                    content: "https://i.imgur.com/hE3wm7M.png",
+                    key: "image"
+                },
+                {
+                    name: "url",
+                    content: "https://www.ohack.org/nonprofits/apply",
+                    key: "url"},
+                {
+                    name: "twitter:card",
+                    content: "summary_large_image",
+                    key: "twittercard"
+                }                
+            ]
+        },
+    };
+};
