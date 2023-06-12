b50ae57e151e4834e9a0576a84f0ef0ec9f08bbf
Greg V <greg.vannoni@gmail.com>
2023-06-11 21:19:39 -0700

    Fix issue with meta tags with NextJS 13 upgrade

--- src/pages/project/[project_id].js
+++ src/pages/project/[project_id].js
@@ -9,3 +9,127 @@ export default function ProjectPage() {
     <Project />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+    ],
+    fallback: "blocking",
+  }
+}
+
+export async function getStaticProps({ params = {} } = {}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/messages/problem_statement/${params.project_id}`
+  );
+  const ps = await res.json();
+
+  var title = "Project: " + ps.title;
+  var metaDescription = ps.status + ": " + ps.description + " ";
+  var countOfhelpingMentors = 0;
+  var countOfhelpingHackers = 0;
+
+  if (ps.helping) {
+    ps.helping.forEach((help) => {
+      if (help.type === "hacker") {
+        countOfhelpingHackers++;
+      } else if (help.type === "mentor") {
+        countOfhelpingMentors++;
+      } else {
+        // Nada
+      }
+    });
+  }
+
+  // Helpful Docs:
+  // https://medium.com/slack-developer-blog/everything-you-ever-wanted-to-know-about-unfurling-but-were-afraid-to-ask-or-how-to-make-your-e64b4bb9254
+  // https://progressivewebninja.com/how-to-setup-nextjs-meta-tags-dynamically-using-next-head/#3-nextjs-dynamic-meta-tags
+  // https://github.com/vercel/next.js/issues/35172#issuecomment-1169362010
+  return {
+    props: {
+      title: title,
+      openGraphData: [
+        {
+          name: "title",
+          content: title,
+          key: "title",
+        },
+        {
+          property: "og:title",
+          content: title,
+          key: "ogtitle",
+        },
+        {
+          name: "description",
+          content: metaDescription,
+          key: "desc",
+        },
+        {
+          property: "og:description",
+          content: metaDescription,
+          key: "ogdesc",
+        },
+        {
+          property: "og:type",
+          content: "website",
+          key: "website",
+        },
+        {
+          property: "og:image",
+          content: "https://i.imgur.com/Ff801O6.png",
+          key: "ogimage",
+        },
+        {
+          property: "twitter:image",
+          content: "https://i.imgur.com/Ff801O6.png",
+          key: "twitterimage",
+        },
+        {
+          property: "og:site_name",
+          content: "Opportunity Hack Developer Portal",
+          key: "ogsitename",
+        },
+        {
+          property: "twitter:card",
+          content: "summary_large_image",
+          key: "twittercard",
+        },
+        {
+          property: "twitter:domain",
+          content: "ohack.dev",
+          key: "twitterdomain",
+        },
+        {
+          property: "twitter:label1",
+          value: "Project Status",
+          key: "twitterlabel1",
+        },
+        {
+          property: "twitter:data1",
+          value: ps.status,
+          key: "twitterdata1",
+        },
+        {
+          property: "twitter:label2",
+          value: "💻 Hackers",
+          key: "twitterlabel2",
+        },
+        {
+          property: "twitter:data2",
+          value: countOfhelpingHackers,
+          key: "twitterdata2",
+        },
+        {
+          property: "twitter:label3",
+          value: "🛟 Mentors",
+          key: "twitterlabel3",
+        },
+        {
+          property: "twitter:data3",
+          value: countOfhelpingMentors,
+          key: "twitterdata3",
+        }
+      ],
+    },
+  };
+}
