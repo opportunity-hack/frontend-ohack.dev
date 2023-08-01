8e5dd432b1501d07e1c5fd19c7dfd699aed412fc
Greg V <greg.vannoni@gmail.com>
2023-08-01 08:05:50 -0700

    Updated form to be closed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
     images: {
         domains: [
             'media0.giphy.com',
+            'media4.giphy.com',
+            'media*.giphy.com',
             'i.imgur.com',
             'avatars.githubusercontent.com',
             'avatars.slack-edge.com',
