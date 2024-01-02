37a36949caf9c2429f44898497d0cdc265c2af3e
Greg V <greg.vannoni@gmail.com>
2024-01-02 09:19:05 -0700

    More LCP hacks for Google Ads grants

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,64 +17,65 @@ export default function Document() {
 
                 {/* Global Site Tag (gtag.js) - Google Analytics */}
                 <script
-                    async
+                    async={true}
                     src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
                 />
+
                 <script
-                            dangerouslySetInnerHTML={{
-                                __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `,
-                            }}
-                />
-                        
-                
-                <script dangerouslySetInnerHTML={{
-                    __html: `!function(f,b,e,v,n,t,s)
-                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                    n.queue=[];t=b.createElement(e);t.async=!0;
-                    t.src=v;s=b.getElementsByTagName(e)[0];
-                    s.parentNode.insertBefore(t,s)}(window, document,'script',
-                    'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '340533780623242');
-                    fbq('track', 'PageView');`
-                }}
+                    async={true}
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                page_path: window.location.pathname,
+                            });
+                        `,
+                    }}
                 />
 
-              
-                
-                
+                <script
+                    async={true}
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                                n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '340533780623242');
+                            fbq('track', 'PageView');
+                        `,
+                    }}
+                />
+
                 <link
-                    rel="preload"
+                    rel="preconnect"
                     as="style"
                     href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter&family=Space+Grotesk&display=optional"
                 />
-           
+
                 <link
-                    rel="preload"
+                    rel="preconnect"
                     href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600&family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600&display=swap"
                     as="style"
                 />
-                    
-                    
-
-                
-                
             </Head>
             <body>
-                
                 <Main />
-                
                 <NextScript />
-                
             </body>
         </Html>
     )
 }
+                
+                
+                        
+
+              
+                
+            
