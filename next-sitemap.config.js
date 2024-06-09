798da7b83cd6da8b9cfc6883499693b45f093d70
Greg V <greg.vannoni@gmail.com>
2024-06-09 11:11:54 -0700

    Added robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
+
+
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.ohack.dev',
-    generateRobotsTxt: false, // (optional)
-    exclude: [ '/admin', '/profile', '/nonprofit/[nonprofit_id]', '/hackathon/[hackathon_id]', '/project/[project_id]', '/hack/[event_id]' ]
+    generateRobotsTxt: true, 
+    exclude: [ '/admin', '/profile', '/nonprofit/[nonprofit_id]', '/hackathon/[hackathon_id]', '/project/[project_id]', '/hack/[event_id]' ],
     // ...other options
 }
