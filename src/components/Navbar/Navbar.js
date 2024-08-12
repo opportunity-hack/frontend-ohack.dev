82c7a6f85503698ee2bfc1af7551f96030dde121
Greg V <greg.vannoni@gmail.com>
2024-08-12 09:05:59 -0700

    Added process page too

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -69,6 +69,9 @@ const about_settings = [
   [ '❤️ Rewards', '/about/hearts' ],
   [ '✅ Project Completion', '/about/completion' ],
  
+  // Process
+  [ '📝 Process', '/about/process' ],
+ 
   [ '🚪 Office Hours', '/office-hours' ],
   // Style Guide
   [ '🎨 Style Guide', '/about/style-guide' ],
